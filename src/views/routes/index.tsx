3fd6b090ab35a21843b5d55718f65a154bba5925
ahmad mianbandi <ahmad.mianbandi@consolsys.com>
2021-07-22 14:10:36 +0800

    changes routing to lazy

--- src/views/routes/index.tsx
+++ src/views/routes/index.tsx
@@ -1,22 +1,25 @@
+import { lazy, Suspense } from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
-import InitStep from '../pages/steps/init-app';
-import AboutProject from '../pages/steps/init-app/about-project';
-import LazyLoadingStep from '../pages/steps/patters/lazy';
-import RouterStep from '../pages/steps/router';
 import './styles.css';
 
+const InitStep = lazy(() => import('../pages/steps/init-app'));
+const AboutProject = lazy(() => import('../pages/steps/init-app/about-project'));
+const LazyLoadingStep = lazy(() => import('../pages/steps/patters/lazy'));
+const RouterStep = lazy(() => import('../pages/steps/router'));
 
 const Routes = () => {
   return (
     <div className="App">
       <header className="App-header"></header>
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/"><InitStep /></Route>
-          <Route path="/step/about-project"><AboutProject /></Route>
-          <Route path="/step/router"><RouterStep /></Route>
-          <Route path="/step/patterns/lazy"><LazyLoadingStep /></Route>
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/"><InitStep /></Route>
+            <Route path="/step/about-project"><AboutProject /></Route>
+            <Route path="/step/router"><RouterStep /></Route>
+            <Route path="/step/patterns/lazy"><LazyLoadingStep /></Route>
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </div>
   )
