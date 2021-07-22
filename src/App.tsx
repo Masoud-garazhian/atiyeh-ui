a44888069f99c86e9027ee2c94b0a2a4fd60eb14
ahmad mianbandi <ahmad.mianbandi@consolsys.com>
2021-07-22 13:17:30 +0800

    modifies app tsx ->  create react app command added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <blockquote><i>"It starts with, one thing":</i></blockquote>
+          <p><code>npx create-react-app preactice --template typescript</code></p>
+          <p>and it makes&nbsp;
+            <a href="https://github.com/aronmi/preactice/tree/ab8c213119a77db27fefb6ddf8b0cc955ad2c474" target="_blank">this</a>
+          </p>
+          <p>
+            <span>see more&nbsp;</span>
+            <a href="https://create-react-app.dev/docs/adding-typescript/" target="_blank" >here</a>
+          </p>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
