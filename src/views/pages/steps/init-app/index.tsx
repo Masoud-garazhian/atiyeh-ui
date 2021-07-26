e12c6a9cf81c2619c5ec3af8eac009c13a4b28ee
masoud garazhian <masoud.garazhian@gmail.com>
2021-07-26 22:52:50 +0430

    change sfc to cc

--- src/views/pages/steps/init-app/index.tsx
+++ src/views/pages/steps/init-app/index.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React,{ Component } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,87 +12,123 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+
+
+
+export interface InitStepProps {
+  
+}
+ 
+export interface InitStepState {
+  
+}
+ 
+class InitStep extends React.Component<InitStepProps, InitStepState> {
+  state = { isActiveOtp : false  }
+   useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  classes = this.useStyles();
+
+   renderOtpTextfield = () =>
+    this.state.isActiveOtp ? "block" :"none";
+
+  render() {  
+    return (
+      
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={this.classes.paper}>
+          <Avatar className={this.classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            ورود به سامانه
+          </Typography>
+          <form className={this.classes.form} noValidate>
+            <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <TextField
+                  variant="standard"
+                  required
+                  fullWidth
+                  id="MobileNo"
+                  label="شماره تلفن"
+                  name="MobileNo"
+                  autoComplete="tel"
+                />
+              </Grid>            
+              <Grid item xs={12}>
+              {/* <Box component="div" display={this.renderOtpTextfield}>  */}
+                <TextField
+                  variant="standard"
+                  required
+                  fullWidth
+                  id="OtpCode"
+                  label="کد دریافتی"
+                  name="OtpCode"
+                  autoComplete="tel"
+                />
+                {/* </Box> */}
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={this.classes.submit}
+              onClick={() => { alert('clicked') }}
+            >
+              دریافت کد
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  درحال حاضر دارای اکانت هستم. ورود
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
+    );
+  }
+}
+ 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Todo: change this :D 
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
   );
 }
+export default InitStep;
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
 
-export default function InitStep() {
-  const classes = useStyles();
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          ورود به سامانه
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="standard"
-                required
-                fullWidth
-                id="MobileNo"
-                label="شماره تلفن"
-                name="MobileNo"
-                autoComplete="tel"
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            دریافت کد
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                درحال حاضر دارای اکانت هستم. ورود
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+
---
+++ src/views/pages/steps/init-app/test.tsx
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+
+export interface testProps {
+    
+}
+ 
+export interface testState {
+    
+}
+ 
+class test extends React.Component<testProps, testState> {
+    state = { xxx:"hi"  }
+    render() { 
+        return (<></>  );
+    }
+}
+ 
+export default test;
